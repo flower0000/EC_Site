031a4120716b343b10f1f74d390543fdecc3b70a
flower0000 <knaoki81@gmail.com>
2022-02-05 23:03:35 +0000

    管理者管理画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,17 @@ devise_for :customers, controllers: {
   passwords:     'customers/passwords',
   registrations: 'customers/registrations'
 }
+
+#homesコントローラー
   root to: 'homes#top'
+  get '/about' => 'homes#about', as: 'about'
+
+ #管理者側コントローラー
+ namespace :admin do
+   #get '/' => 'homes#top', as: '' 名前付きルートadmin_pathにしたければこれでも
+   root to: 'homes#top'
+   resources :items, only:[:index, :create, :new, :edit, :show, :update]
+   resources :customers, only:[:index, :show, :edit, :update]
+ end
 
 end
